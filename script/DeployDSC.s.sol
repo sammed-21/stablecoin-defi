f2e1eebe069346040ac163235f7eb38ff4e85891
sammed-21 <sammed2110@gmail.com>
2026-01-16 23:48:54 +0530

    adde the test to mock price feed

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -7,25 +7,34 @@ import {DecentralizedStableCoint} from "../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../src/DSCEngin.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 
-
-
 contract DeployDSC is Script {
     address[] public tokenAddresses;
     address[] public priceFeedAddresses;
-    function run() external returns(DecentralizedStableCoin, DSCEngine){
+
+    function run() external returns (DecentralizedStableCoin, DSCEngine) {
         HelperConfig config = new HelperConfig();
 
-        {address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey} = config.activeNetworkConfig();
-     
-     tokenAddresses = [weth, wbtc];
-     priceFeedAddresses = [wethUsdPriceFeed,wbtcUsdPriceFeed];
+        (
+            address wethUsdPriceFeed,
+            address wbtcUsdPriceFeed,
+            address weth,
+            address wbtc,
+            uint256 deployerKey
+        ) = config.activeNetworkConfig();
+
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
         vm.startBroadcast();
 
         DecentralizedStableCoin dsc = new DecentralizedStableCoint();
-        DSCEngine engine = new DSCEngine(tokenAddresses, priceFeedAddresses,address(dsc));
+        DSCEngine engine = new DSCEngine(
+            tokenAddresses,
+            priceFeedAddresses,
+            address(dsc)
+        );
         dsc.transferOwnership(address(engine));
 
         vm.stopBroadcast();
-        return (dsc,engine);
+        return (dsc, engine, config);
     }
 }
--- test/unit/DSCEngine.t.sol
+++ test/unit/DSCEngine.t.sol
@@ -3,14 +3,31 @@
 pragma solidity ^0.8.18;
 
 import {Test} from "forge-std/Test.sol";
+import {console} from "forge-std/console.sol";
 import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../../src/DSCEngin.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC deployer;
+    DecentralizedStableCoin dsc;
+    DSCEngine dsc;
+    HelperConfig config;
+    address ethUsdPriceFeed;
+    address weth;
 
-    functoin setUp() public {
+    function setUp() public {
         deployer = new DeployeDSC();
-
+        (dsc, dsce, config) = deployer.run();
+        (ethUsdPriceFeed, , weth, , ) = config.activeNetworkConfig();
     }
 
+    /////////////////
+    // Price Tests //
+    /////////////////
+
+    function testGetUsdValue() public {
+        console.log(ethUsdPriceFeed, weth);
+    }
 }
